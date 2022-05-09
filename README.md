14026f118541f22234e7bbac64cb6f5d31b52ae4
Mark John Merin <v-mamer@microsoft.com>
2022-05-09 10:35:43 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Azure CLI Library
+
+This repo contains a `ruby` library, wrapping some of the basic azure-cli commands. 
+
+## Dependencies
+* [Azure CLI](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
+* [json](https://rubygems.org/gems/json)
+* [safe_shell](https://github.com/envato/safe_shell)
+
+## Libraries
+
+### WorkItem
+
+Wraps the [az boards work-item show](https://docs.microsoft.com/en-us/cli/azure/boards/work-item?view=azure-cli-latest#az-boards-work-item-show) command. 
+
+Useful for querying work items on the Az boards for reporting purposes.
+
---
+++ workitem.rb
@@ -0,0 +1,87 @@
+require 'json'
+require 'safe_shell'
+
+class WorkItem 
+
+    attr_reader :title
+    attr_reader :createdBy
+    attr_reader :children
+    attr_reader :parent
+    attr_reader :tags
+    attr_reader :item_id
+    attr_reader :createdDate
+    attr_reader :state
+    attr_reader :type
+    attr_reader :url
+    attr_reader :project
+
+    def initialize(org, itemid)
+        @org = org
+        @item_id = itemid
+        @az_cmd = '/usr/local/bin/az'
+        @datahash = JSON.parse(az_cmd_response)
+        @createdDate = get_createdDate
+        @title = get_title
+        @createdBy = get_createdby
+        @children = get_relations("Child")
+        @parent = get_relations("Parent")
+        @tags = get_tags
+        @state = get_state
+        @type = get_type
+        @url = build_url
+        @project = get_project
+    end
+
+    def get_title
+        @datahash["fields"]["System.Title"]
+    end
+
+    def get_type
+        @datahash["fields"]["System.WorkItemType"]
+    end
+
+    def get_tags
+        @datahash["fields"]["System.Tags"].nil? ? "" : @datahash["fields"]["System.Tags"]
+    end
+
+    def get_state
+        @datahash["fields"]["System.State"]
+    end
+
+    def get_project
+        @datahash["fields"]["System.TeamProject"]
+    end
+
+    def get_createdby
+        [@datahash["fields"]["System.CreatedBy"]["displayName"], @datahash["fields"]["System.CreatedBy"]["uniqueName"]]
+    end
+
+    def get_createdDate
+        @datahash["fields"]["System.CreatedDate"]
+    end
+
+    def get_relations(relationship)
+        relations = @datahash["relations"]
+        relationship_array = Array.new
+        relations.each do |relation|    
+            if relation["attributes"]["name"].eql? relationship
+                ticket_url = relation["url"]
+                ticket_id_regex = ticket_url.match /workItems\/(.*)/
+                ticket_id = ticket_id_regex[1]
+                relationship_array.push ticket_id
+            end
+        end
+        relationship_array
+    end 
+
+    def build_url
+        File.join @org, get_project, '_workitems', 'edit', @item_id
+    end
+
+    private
+
+    def az_cmd_response
+        SafeShell.execute!("#{@az_cmd}", "boards", "work-item", "show", "--id", "#{@item_id}", "--org", "#{@org}") 
+    end
+
+end
